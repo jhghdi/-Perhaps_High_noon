664b83357e1b013f4f97d35a658c00f3776a19bd
Jung Hokyun <jhghdi@gmail.com>
2016-06-28 14:05:51 +0900

    note test scene(GameScene) 추가

---
+++ PerHaps High Noon/Assets/02.Scripts/GameScene/csPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class csPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag.Equals("Note"))
+                {
+                    csNote note = hit.transform.GetComponentInParent<csNote>();
+                    if (note != null)
+                        note.OnTrigger();
+                }
+            }
+        }
+    }
+}
---
+++ PerHaps High Noon/Assets/02.Scripts/MainScene/csPause.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class csPause : MonoBehaviour {
+	public GameObject pauseCanvas;
+
+
+	public void OnPause(){
+		pauseCanvas.SetActive (true);
+		Time.timeScale = 0;
+	}
+
+	public void OnCancel(){
+		pauseCanvas.SetActive (false);
+		Time.timeScale = 1.0f;
+	}
+
+	public void OnRetire(){
+		SceneManager.LoadScene ("StageScene");
+	}
+}
