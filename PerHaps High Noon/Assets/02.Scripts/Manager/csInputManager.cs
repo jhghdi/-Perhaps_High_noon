c99e42849fcccac60421ba38355cc2bd007bf733
ungrave <rlaekgns14@naver.com>
2016-07-04 10:00:21 +0900

    변경사항
    플랫폼 조건문 제거 및 그에 따른 추가 수정
    .touches.length = 0 시에 따른 조건
    csPause 의 기능을 inputManager에 병합

--- PerHaps High Noon/Assets/02.Scripts/Manager/csInputManager.cs
+++ PerHaps High Noon/Assets/02.Scripts/Manager/csInputManager.cs
@@ -1,11 +1,7 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class csInputManager : MonoBehaviour {
-
-	/// <summary>
-	/// Platform 정의
-	/// </summary>
-	enum PLATFORM { ANDROID = 0, EDITOR = 1 };
+	public GameObject pauseCanvas;
 
 	/// <summary>
 	/// Player 객체
@@ -17,67 +13,48 @@ public class csInputManager : MonoBehaviour {
 	/// </summary>
 	csPlayer playerMethod;
 
-	/// <summary>
-	/// 현재 platform
-	/// </summary>
-	PLATFORM platform;
-
 	// Use this for initialization
 	void Start () {
 		playerMethod = player.GetComponent<csPlayer>();
-
-		if (Application.platform == RuntimePlatform.Android)
-			platform = PLATFORM.ANDROID;
-		else
-			platform = PLATFORM.EDITOR;
-	}
-
-	// Update is called once per frame
-	void Update() {
-
-		// 플랫폼에 따른 인식 확인
-		if (platform == PLATFORM.ANDROID && Input.touches != null)
-			DoTouch();
-
-		else if (platform == PLATFORM.EDITOR)
-			DoClick();
 	}
 
 	/// <summary>
 	/// touch의 행동을 체크한다.
 	/// </summary>
-	private void DoTouch()
-	{
-		Touch touch = Input.touches[0];
-		Vector3 position = touch.position;
-		Common.INPUT inputStatus = Common.INPUT.INPUT_BEGIN;
+	void Update() {
+		if (Input.touches.Length > 0) {
+			Touch touch = Input.touches [0];
+			Vector3 position = touch.position;
+			Common.INPUT inputStatus = Common.INPUT.INPUT_BEGIN;
 
-		if (touch.phase == TouchPhase.Began)
-			inputStatus = Common.INPUT.INPUT_BEGIN;
-		else if (touch.phase == TouchPhase.Moved)
-			inputStatus = Common.INPUT.INPUT_MOVE;
-		else if (touch.phase == TouchPhase.Ended)
-			inputStatus = Common.INPUT.INPUT_END;
+			if (touch.phase == TouchPhase.Began)
+				inputStatus = Common.INPUT.INPUT_BEGIN;
+			else if (touch.phase == TouchPhase.Moved)
+				inputStatus = Common.INPUT.INPUT_MOVE;
+			else if (touch.phase == TouchPhase.Ended)
+				inputStatus = Common.INPUT.INPUT_END;
 
-		playerMethod.DoAction(position, inputStatus);
+			playerMethod.DoAction (position, inputStatus);
+		}
 	}
 
-	/// <summary>
-	/// mouse click이나 drag를 체크한다.
-	/// </summary>
-	private void DoClick()
-	{
-		Vector3 position = Input.mousePosition;
+	public void OnPause(){
 
-		if (Input.GetButtonDown("Fire1"))
-			playerMethod.DoAction(position, Common.INPUT.INPUT_BEGIN);
-		else if (Input.GetButton("Fire1"))
-			playerMethod.DoAction(position, Common.INPUT.INPUT_MOVE);
-		else if (Input.GetButtonUp("Fire1"))
-			playerMethod.DoAction(position, Common.INPUT.INPUT_END);
-		else
-			return;
+		pauseCanvas.SetActive (true);
+		Time.timeScale = 0;
 	}
 
+	public void OnCancel(){
+		pauseCanvas.SetActive (false);
+		Time.timeScale = 1.0f;
+	}
+
+	public void OnRetire(){
+		Time.timeScale = 1.0f;
+		GameObject[] g = GameObject.FindObjectsOfType<GameObject> ();
+		foreach(GameObject gg in g){
+			Destroy (gg);
+		}
+		SceneManager.LoadScene ("StageScene");
+	}
 }
-
